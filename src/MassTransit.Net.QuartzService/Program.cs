7bd7ed3b0bcb1054a0e8c2e58f885d2bac9b40b2
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-05-11 12:00:37 -0500

    Empty MassTransit.Net.QuartzService

---
+++ src/MassTransit.Net.QuartzService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MassTransit.Net.QuartzService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
