67ef4b2bdccf34a1611f20526d4f6343ed0ed62a
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-05-11 12:50:59 -0500

    Demo MassTransit.Net.QuartzService

--- src/MassTransit.Net.QuartzService/Program.cs
+++ src/MassTransit.Net.QuartzService/Program.cs
@@ -1,12 +1,100 @@
-﻿using System;
+﻿using GreenPipes;
+using MassTransit;
+using MassTransit.QuartzIntegration;
+using MassTransit.Scheduling;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MassTransit.Net.QuartzService
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            var builder = CreateHostBuilder(args);
+
+            if (isService)
+            {
+                await builder.UseWindowsService().Build().RunAsync();
+            }
+            else
+            {
+                await builder.RunConsoleAsync();
+            }
         }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            new HostBuilder()
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile("appsettings.json", optional: true);
+                config.AddEnvironmentVariables();
+
+                if (args != null)
+                    config.AddCommandLine(args);
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.Configure<AppConfig>(hostContext.Configuration.GetSection("AppConfig"));
+                services.Configure<QuartzConfig>(hostContext.Configuration.GetSection("quartz"));
+
+                // Service Bus
+                services.AddMassTransit(cfg =>
+                {
+                    cfg.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                    {
+                        var options = provider.GetRequiredService<IOptions<AppConfig>>().Value;
+                        var scheduler = provider.GetRequiredService<IScheduler>();
+
+
+                        var host = cfg.Host(options.Host, options.VirtualHost, h =>
+                        {
+                            h.Username(options.Username);
+                            h.Password(options.Password);
+                        });
+
+                        cfg.UseJsonSerializer(); // Because we are using json within Quartz for serializer type
+
+                        cfg.ReceiveEndpoint(options.QueueName, endpoint =>
+                        {
+                            var partitionCount = Environment.ProcessorCount;
+                            endpoint.PrefetchCount = (ushort)(partitionCount);
+                            var partitioner = endpoint.CreatePartitioner(partitionCount);
+
+                            endpoint.Consumer(() => new ScheduleMessageConsumer(scheduler), x =>
+                                x.Message<ScheduleMessage>(m => m.UsePartitioner(partitioner, p => p.Message.CorrelationId)));
+                            endpoint.Consumer(() => new CancelScheduledMessageConsumer(scheduler),
+                                x => x.Message<CancelScheduledMessage>(m => m.UsePartitioner(partitioner, p => p.Message.TokenId)));
+                        });
+                    }));
+                });
+
+                services.AddHostedService<MassTransitConsoleHostedService>();
+
+                services.AddSingleton(x =>
+                {
+                    var quartzConfig = x.GetRequiredService<IOptions<QuartzConfig>>().Value
+                        .UpdateConnectionString(hostContext.Configuration.GetConnectionString("scheduler-db"))
+                        .ToNameValueCollection();
+                    return new StdSchedulerFactory(quartzConfig).GetScheduler().ConfigureAwait(false).GetAwaiter().GetResult();
+
+                });
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+            });
     }
 }
---
+++ src/MassTransit.Net.QuartzService/QuartzConfig.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace MassTransit.Net.QuartzService
+{
+    public class QuartzConfig : Dictionary<string, string>
+    {
+        public QuartzConfig UpdateConnectionString(string connectionString)
+        {
+            this["quartz.dataSource.quartzDS.connectionString"] = connectionString;
+            return this;
+        }
+
+        public NameValueCollection ToNameValueCollection()
+        {
+            return this.Aggregate(new NameValueCollection(), (seed, current) =>
+            {
+                seed.Add(current.Key, current.Value);
+                return seed;
+            });
+        }
+    }
+}
