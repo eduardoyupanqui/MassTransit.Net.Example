17138453eae989a1e252ae0828771336cfc8e216
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-05-18 23:09:14 -0500

    EndpointConvention para ConsumeContext.Send

--- src/MassTransit.Net.Jobs.Client/Extensions/IRabbitMqBusFactoryConfiguratorExtensions.cs
+++ src/MassTransit.Net.Jobs.Client/Extensions/IRabbitMqBusFactoryConfiguratorExtensions.cs
@@ -1,4 +1,5 @@
-﻿using MassTransit.Net.Jobs.Client.Consumers;
+﻿using MassTransit.Net.Jobs.Client.Commands;
+using MassTransit.Net.Jobs.Client.Consumers;
 using MassTransit.RabbitMqTransport;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,10 @@ namespace MassTransit.Net.Jobs.Client.Extensions
         public static void ReceivedJobEndpoint<T>(this IRabbitMqBusFactoryConfigurator cfg, IServiceProvider provider) 
             where T : BaseExecutor
         {
+            EndpointConvention.Map<JobStarted>(new Uri($"queue:{typeof(JobStarted).Name.ToUnderscoreCase()}"));
+            EndpointConvention.Map<JobTaskCompleted>(new Uri($"queue:{typeof(JobTaskCompleted).Name.ToUnderscoreCase()}"));
+            EndpointConvention.Map<JobCompleted>(new Uri($"queue:{typeof(JobCompleted).Name.ToUnderscoreCase()}"));
+            EndpointConvention.Map<JobFailed>(new Uri($"queue:{typeof(JobFailed).Name.ToUnderscoreCase()}"));
             cfg.ReceiveEndpoint(typeof(T).Name.ToUnderscoreCase(), ep =>
             {
                 ep.Consumer<JobConsumer<T>>(provider);
