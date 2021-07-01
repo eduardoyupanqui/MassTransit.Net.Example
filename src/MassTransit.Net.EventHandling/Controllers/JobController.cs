3337dd5fcea88adc21ca38a7c65c7435d9433b25
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2021-07-01 01:10:46 -0500

    Add MassTransit.Net.EventHandling

---
+++ src/MassTransit.Net.EventHandling/Controllers/JobController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MassTransit.Net.EventHandling.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobController : ControllerBase
+    {
+        private readonly ILogger<JobController> _logger;
+
+        public JobController(ILogger<JobController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
